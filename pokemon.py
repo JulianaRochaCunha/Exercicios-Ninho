f3e7621cd4ea10d31546a387c391cd8e1ae92839
JulianaRochaCunha <116909492+JulianaRochaCunha@users.noreply.github.com>
2023-01-06 11:33:27 -0300

    Add files via upload

--- pokemon.py
+++ pokemon.py
@@ -1,32 +1,84 @@
+
+import random
 class Pokemon():
     def __init__(self, nome, tipo, hp, level):
         self.nome = nome
         self.tipo = tipo
         self.hp = hp
         self.level = level
+        self.movimento = "Ataque rápido"
 
 
+class Eletrico(Pokemon):
+    def __init__(self, nome, tipo, hp, level):
+        super().__init__(nome, tipo, hp, level)
+        self._tipo = "Eletrico"
+        self._movimento ="Choque eletrico"
 
+class Venenoso(Pokemon):
+    def __init__(self, nome, tipo, hp, level):
+        super().__init__(nome, tipo, hp, level)
+        self._tipo = "Venenoso"
+        self._movimento ="Picada venenosa"
 
-pokemon1 = Pokemon("Pikachu", "Eletrico", "35","1")
-pokemon2 = Pokemon("Nidorino", "Venenoso","70","16")
-pokemon3 = Pokemon("Parasect", "Inseto", "60", "24")
+class Inseto(Pokemon):
+    def __init__(self, nome, tipo, hp, level):
+        super().__init__(nome, tipo, hp, level)
+        self._tipo = "Parasect"
+        self._movimento ="Pó do sono"
 
-while True:
-    Inicio = input("Escolha um Pokemon. Pikachu is 1, Nidorino is 2 and Parasect is 3 ")
-    if Inicio > "3" or Inicio <"1":
-        print("Por favor tente novamente")
+class Treinador:
+    def __init__(self, nome, pokemons):
+        self._nome = nome
+        self._pokemons = pokemons
+
+    def escolhaumpokemon(self):
+        return random.choice(self._pokemons)
+
+class Jogador(Treinador):
+    def __init__(self, nome, pokemons):
+        super().__init__(nome, pokemons)
+
+class Inimigo(Treinador):
+    def __init__(self, nome, pokemons):
+        super().__init__(nome, pokemons)
+
+def batalhadepokemon(treinador1, treinador2):
+
+    p1 = treinador1._escolhaDoPokemon()
+    p2 = treinador2._escolhaDoPokemon()
+    
+
+    print(f"{p1._nome} atacou com {p1._movimento} e força {p1força}")
+    print(f"{p2._nome} atacou com {p2._movimento} e forca {p2força}")
+    
+
+    p1força = p1._hp
+    p2força = p2._hp
+    
+
+    print(f"O vencedor foi {p1.nome}")
+
+    if (p1._hp) > (p2._hp):
+        print(f"O vencedor foi {p1._hp}")
+
+    elif (p2._hp) > (p1._hp):
+        print(f"O vencedor foi {p2._hp}")
+
     else:
-        print("Preparar para a batalha")
-        match Inicio:
-            case "1":
-                print(vars(pokemon1))
-            case "2":
-                print(vars(pokemon2))
-            case "3":
-                print(vars(pokemon3))
-            
-        break
+        print("Empate")
+
+pokemon1 = Eletrico("Pikachu", "Eletrico", 35,1)
+pokemon2 = Venenoso("Nidorino", "Venenoso",70,16)
+pokemon3 = Inseto("Parasect", "Inseto", 60, 24)
+
+jogador = Jogador ("Juliana", [pokemon1])
+inimigo = Inimigo ("Joe", [pokemon2])
+
+batalhadepokemon(jogador,inimigo)
+
+    
+
 
     
 
