ff4ee236637b6a30ce0abbae13bc4f057aa2cbc7
JulianaRochaCunha <116909492+JulianaRochaCunha@users.noreply.github.com>
2023-02-08 09:26:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,26 @@
 
+from Controle.classConexao import Conexao
 import psycopg2
 
-def createTableFuncionarios(cursor,conexao):
-    cursor.execute('''
-    DROP TABLE IF EXISTS "Funcionarios";
-
-    CREATE TABLE "Funcionarios"(
-        "ID" int GENERATED ALWAYS AS IDENTITY,
-        "Nome" Varchar(255) NOT NULL,
-        "Salario" money,
-        "ID_Departamento" int,
-        CONSTRAINT fk_departamento
-            FOREIGN KEY("ID_Departamento")
-            REFERENCES "Departamento"("ID")
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-            
-        PRIMARY KEY ("ID"));
-        ''')
-    conexao.commit()
-
-def createTableDepartamentos(cursor,conexao):
-    cursor.execute('''
-    DROP TABLE IF EXISTS "Departamentos";
-    CREATE TABLE "Departamentos"(
-        "ID" INT GENERATED ALWAYS AS IDENTITY,
-        "Nome" Varchar(255) NOT NULL, 
-        PRIMARY KEY("ID"));''')
-
-def inserirFuncionario(cursor,conexao):
-    
-    cursor.execute('''
-    INSERT INTO "Funcionarios"
-    VALUES(default, 'Joao Miguel', '2.500,00', default)
-    ''')
-    conexao.commit()
-
 try:
+    con = Conexao("Empresa", "localhost", "5432", "postgres", "postgres")
 
-    con = psycopg2.connect(database="Empresa", user="postgres", password="postgres", host="localhost", port="5432")
-    cursor = con.cursor()
+    con.manipularBanco('''
+    INSERT INTO "Funcionarios"
+    Values(default, 'José')
+    ''')
 
-    """createTableDepartamentos(cursor,con)
-    print("Tabela Departamentos criada")
+    print (con.consultarBanco('''
+    SELECT *FROM "Funcionarios"
+    '''))
 
-    createTableFuncionario(cursor,con)
-    cursor.close()
-    con.close()
-    print("Tabela Funcionario criada")"""
+    print(con.consultarBanco('''
+    SELECT * FROM "Funcionarios"
+    '''))
 
-   
-    inserirFuncionario(cursor,con)
-    cursor.close()
-    con.close()
-
-    print("Funcionario inserido")
+    con._db.close()
 
 
-
-
-except(Exception, psycopg2.Error) as error:
-
-    print("Ocorreu um erro", error)
     
+except (Exception, psycopg2.Error) as error:
+    print("Ocorreu um erro:", error)
